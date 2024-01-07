73bc4dcbda38b3ea204554e6340438865e24750b
Rinslep <4941570+Rinslep@users.noreply.github.com>
2024-01-07 21:55:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Importing the necessary libraries
 from pprint import pprint
 from blueprintMap import *
+from slpp import slpp as lua
 import calculator
 
 # load the blueprint string from the file
@@ -11,10 +12,10 @@ with open('blueprint.txt', 'r') as file:
 blueprint_dict = blueprint_string_to_dict(blueprint_string)
 
 # create a list of entities
-entities = blueprint_dict['blueprint']['entities']
+bp_entities = blueprint_dict['blueprint']['entities']
 
 # create a list of nodes and edges from the entities
-for entity in entities:
+for entity in bp_entities:
     entity_to_nodes_and_edges(entity)
 
 nodes = Node.node_dict.values()
@@ -29,3 +30,65 @@ map_array = map.get_map_array()
 # pprint(map.get_map_array())
 #
 # pprint(Node.similar_nodes)
+
+data = lua.decode(open('data-final-fixes.lua').read())
+
+import_category = [
+    "accumulator",
+    "ammo-turret",
+    "assembling-machine",
+    "beacon",
+    "boiler",
+    "curved-rail",
+    "decider-combinator",
+    "electric-energy-interface",
+    "electric-pole",
+    "electric-turret",
+    "fluid-turret",
+    "furnace",
+    "gate",
+    "generator",
+    "heat-interface",
+    "heat-pipe",
+    "inserter",
+    "lab",
+    "lamp",
+    "land-mine",
+    "linked-belt",
+    "loader",
+    "logistic-container",
+    "mining-drill",
+    "offshore-pump",
+    "pipe",
+    "pipe-to-ground",
+    "power-switch",
+    "programmable-speaker",
+    "pump",
+    "radar",
+    "rail-chain-signal",
+    "rail-signal",
+    "reactor",
+    "roboport",
+    "rocket-silo",
+    "solar-panel",
+    "splitter",
+    "storage-tank",
+    "straight-rail",
+    "train-stop",
+    "transport-belt",
+    "underground-belt",
+    "wall"
+]
+
+placeables = {}
+
+
+for category in import_category:
+    # pprint(data[category])
+    for key, value in data[category].items():
+        placeables[key] = value
+
+pprint(data)
+
+
+
